f90e3c3864f2cb8cfbb21c700636a8b8aae62488
Jordan Gensler <jgensler@netflix.com>
2019-12-18 16:33:14 -0800

    Fix build

--- packages/backend/src/typeDefs.ts
+++ packages/backend/src/typeDefs.ts
@@ -3,5 +3,6 @@ import fs from 'fs';
 import path from 'path';
 
 export default gql(
-    fs.readFileSync(path.join(__dirname, 'schema.graphql'), 'utf8'),
+    // TODO: Move this back to src? I need to copy the file manually if I do that as part of build.
+    fs.readFileSync(path.join(__dirname, '..', 'schema.graphql'), 'utf8'),
 );
