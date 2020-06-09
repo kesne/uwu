02bc4b981fb77c9f89ef2c7b744c42fdcb2e64be
Jordan Gensler <jgensler@netflix.com>
2020-06-09 12:08:36 -0700

    Hello

--- packages/backend/ormconfig.js
+++ packages/backend/ormconfig.js
@@ -1,9 +1,11 @@
 const isProd = process.env.NODE_ENV === 'production';
 
+console.log({ isProd });
+
 module.exports = {
     type: 'postgres',
     url: process.env.DATABASE_URL || 'postgresql://postgres:uwu@uwu-postgres/postgres',
-    logging: false /* !isProd */,
+    logging: true /* !isProd */,
     synchronize: !isProd,
     entities: [isProd ? 'lib/entity/*.js' : 'src/entity/*.ts'],
     migrations: [isProd ? 'lib/migration/*.js' : 'src/migration/*.ts'],
